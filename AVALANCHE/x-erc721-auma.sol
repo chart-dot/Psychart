8c2dc716ad53a5d2e92763204c9f2d7d45debd0f
chart-dot <atom139139@gmail.com>
2024-08-31 18:50:23 -0300

    Update x-erc721-auma.sol

--- AVALANCHE/x-erc721-auma.sol
+++ AVALANCHE/x-erc721-auma.sol
@@ -3,10 +3,13 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts@4.8.1/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts@4.8.1/token/ERC721/extensions/ERC721Burnable.sol";   
-  
 
+import "@openzeppelin/contracts@4.8.1/access/AccessControl.sol";   
+ // Importa AccessControl
 
-contract ValidationNFTBridge is ERC721, ERC721Burnable {
+contract ValidationNFTBridge is ERC721, ERC721Burnable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE"); // Rol para mintear
+
     struct TestResult {
         uint256 testId;
         uint256 timestamp;
@@ -20,28 +23,17 @@ contract ValidationNFTBridge is ERC721, ERC721Burnable {
     string private constant _TOKEN_SYMBOL = "AUMA";
     string private constant _TOKEN_URI = "https:/example.com/ipfs/";
 
-    constructor() ERC721(_TOKEN_NAME, _TOKEN_SYMBOL) {}
+    constructor() ERC721(_TOKEN_NAME, _TOKEN_SYMBOL) {
+        _setupRole(MINTER_ROLE, msg.sender); // Otorgar rol de minteo al deployer
+    }
 
-    function mintNFT(uint256 tokenId, uint256 testId, uint256 score) public {
+    function mintNFT(uint256 tokenId, uint256 testId, uint256 score) public onlyRole(MINTER_ROLE) {
         require(_ownerOf(tokenId) == address(0), "Token already minted");
         _mint(msg.sender, tokenId);
 
         testResults[tokenId] = TestResult(testId, block.timestamp, score);
     }
 
-    function getTestResult(uint256 tokenId) public view returns (TestResult memory) {
-        require(_exists(tokenId), "Token does not exist");
-        return testResults[tokenId];
-    }
+    // ... otras funciones (mantener validación de datos, etc.)
 
-    function _baseURI() internal view virtual override returns (string memory) {
-        return _TOKEN_URI;
-    }
-
-    function baseUri() external pure returns (string memory) {
-        return _TOKEN_URI;
-    }
-
-    // Funciones adicionales para bridge (opcional)
-    // ... (código para interactuar con el puente)
-}
+    // ... funciones adicionales para bridge (opcional)
